53c0695968654ad49e32de1eba46f101f9ed6401
medelhachimii <elhachimimohammed94@gmail.com>
2023-08-10 06:27:28 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a programme that  prints a line with puts function
+ * return: 0 if exited properly, non zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
