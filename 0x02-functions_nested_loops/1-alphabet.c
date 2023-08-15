0cd66ba133044d7fe62f9a39ee7eb27f3f6f4a48
medelhachimii <elhachimimohammed94@gmail.com>
2023-08-15 04:28:08 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,14 +6,12 @@
 
 void print_alphabet(void)
 {
-	char alph;
-
-	alph = 'a';
+	char alph = 'a';
 
 	while (alph <= 'z')
 	{
 		_putchar('alph');
 		alph++;
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
