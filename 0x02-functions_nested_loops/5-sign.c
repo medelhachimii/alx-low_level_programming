fc9ba274d893f46f43ebc0e772e476223a4ec199
medelhachimii <elhachimimohammed94@gmail.com>
2023-08-16 14:44:04 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_sign :print the sign of a number is
+ * print_sign - print the sign of a number is
  * greater, equal or less than 0
  * @n: input number
  * Return: 1 if greater, 0 if equal or -1 if less than zero
