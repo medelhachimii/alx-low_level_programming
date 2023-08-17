b4c8d7b9a5063287c87206588749cf429ba77941
medelhachimii <elhachimimohammed94@gmail.com>
2023-08-17 12:46:29 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - check main
+ * @r: integre input
+ * this function return absloute value of a number
+ * Return: Absolute value of number r
+ */
+int_abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
+
