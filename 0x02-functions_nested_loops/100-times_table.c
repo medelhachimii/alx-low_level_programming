f2409849571f3cea6089a734c3e703d0cbf85f6b
medelhachimii <elhachimimohammed94@gmail.com>
2023-08-18 05:36:28 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,14 +28,17 @@ void print_times_table(int n)
 				{
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(' ');
 					_putchar(k + '0');
 				} else if (k < 100)
 				{
+					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				} else
 				{
+					_putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
