5640c9058f7d7cc4ea732e0510a6c00824e966c1
medelhachimii <elhachimimohammed94@gmail.com>
2023-08-24 13:14:20 -0700

    100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - finds and prints the larget prime factor of the number 612852475143
+ * followed by a new line
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	long int n;
+	long int max;
+	long int i;
+
+	n = 612852475143;
+	max = -1;
+
+	while (n % 2 == 0)
+	{
+		max = 2;
+		n /= 2;
+	}
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
+
+	if (n > 2)
+		max = n;
+	printf("%ld\n", maw);
+
+	return (0);
+}
